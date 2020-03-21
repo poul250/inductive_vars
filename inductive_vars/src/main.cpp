5fda6e9fb7a361817d753dc7ac5ea39134ed15fc
Kulis Pavel <poul250@yandex.ru>
2020-03-21 22:14:16 +0300

    Add Loop structure and renamae Cycle -> Loop + some format

--- inductive_vars/src/main.cpp
+++ inductive_vars/src/main.cpp
@@ -19,6 +19,10 @@ struct edge_hash {
 };
 using Edges = std::unordered_set<Edge, edge_hash>;
 
+struct Loop {
+    Blk* head;
+    std::unordered_set<Blk*> blocks;
+};
 
 Edges FindBackEdges(Blk *start) {
     Edges back_edges;
@@ -34,52 +38,52 @@ Edges FindBackEdges(Blk *start) {
     return back_edges;
 }
 
-void ExtendWithCycleBlocks(std::unordered_set<Blk*>& blocks, Blk* cur_block) {
+void ExtendWithLoopBlocks(std::unordered_set<Blk*>& blocks, Blk* cur_block) {
     blocks.insert(cur_block);
     const auto& pred_blocks = cur_block->pred;
     for (int i = 0; i < cur_block->npred; ++i) {
         if (blocks.find(pred_blocks[i]) == blocks.cend()) {
-            ExtendWithCycleBlocks(blocks, pred_blocks[i]);
+            ExtendWithLoopBlocks(blocks, pred_blocks[i]);
         }
     }
 }
 
-std::unordered_set<Blk*> FindCycleBlocks(const Edge& back_edge) {
-    const auto& [start_block, cycle_head] = back_edge;
-    std::unordered_set<Blk*> blocks = {cycle_head};
+std::unordered_set<Blk*> FindLoopBlocks(const Edge& back_edge) {
+    const auto& [start_block, loop_head] = back_edge;
+    std::unordered_set<Blk*> blocks = {loop_head};
 
-    ExtendWithCycleBlocks(blocks, start_block);
+    ExtendWithLoopBlocks(blocks, start_block);
     return blocks;
 }
 
-std::vector<std::unordered_set<Blk*>> FindCycles(Blk *start) {
+std::vector<std::unordered_set<Blk*>> FindLoops(Blk *start) {
     std::vector<std::unordered_set<Blk*>> result;
     const auto& back_edges = FindBackEdges(start);
 
     for (const auto& back_edge : back_edges) {
-        auto&& blocks = FindCycleBlocks(back_edge);
+        auto&& blocks = FindLoopBlocks(back_edge);
         result.emplace_back(std::move(blocks));
     }
 
     return result;
 }
 
-static void readfn (Fn *fn) {
+static void readfn(Fn *fn) {
     printfn(fn, stdout);
-    const auto& cycles = FindCycles(fn->start);
-    for (const auto& cycle : cycles) {
-        for (const auto& block : cycle) {
+    const auto& loops = FindLoops(fn->start);
+    for (const auto& loop : loops) {
+        for (const auto& block : loop) {
             std::cout << block->id << " ";
         }
         std::cout << std::endl;
     }
 }
 
-static void readdat (Dat *dat) {
+static void readdat(Dat *dat) {
     (void) dat;
 }
 
-int main (int argc, char ** argv) {
+int main(int argc, char ** argv) {
     parse(stdin, "<stdin>", readdat, readfn);
     freeall();
 }
